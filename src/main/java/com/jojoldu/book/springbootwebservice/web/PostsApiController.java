ff58787d80b9e57535e7e611c623fec723f5ef55
최주아 사원 DWP서비스팀 (zuaseyo@lgcns.com <zuaseyo@CNS.LGCNS.COM>
2022-07-06 18:05:32 +0900

    오류

--- src/main/java/com/jojoldu/book/springbootwebservice/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springbootwebservice/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springbootwebservice.web;
 import com.jojoldu.book.springbootwebservice.domain.posts.PostsService;
 import com.jojoldu.book.springbootwebservice.web.dto.PostsResponseDto;
 import com.jojoldu.book.springbootwebservice.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,11 @@ public class PostsApiController {
     }
 
     @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
     }
